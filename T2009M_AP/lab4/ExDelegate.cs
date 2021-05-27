0641b71698b704b0e408a91c003a11c9a3a7ada2
Linh Nguyen <libh1004@gmail.com>
2021-05-27 09:28:33 +0700

    nop lab 4

---
+++ T2009M_AP/lab4/ExDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T2009M_AP.lab4
+{
+    public class ExDelegate
+    {
+        public static void ShowInfo(int num1, int num2)
+        {
+            Console.WriteLine("num1: "+num1);
+            Console.WriteLine("num2: "+num2);
+        }
+        public void AddNumber(int num1, int num2)
+        {
+            Console.WriteLine("Tong 2 so: "+(num1 + num2));
+        }
+        public void SubNumber(int num1, int num2)
+        {
+            Console.WriteLine("Hieu 2 so: "+(num1 - num2));
+        }
+        public void MulNumber(int num1, int num2)
+        {
+            Console.WriteLine("Tich 2 so: "+(num1 * num2));
+        }
+        public void DivNumber(int num1, int num2)
+        {
+            Console.WriteLine("Thuong 2 so: "+(num1 / num2));
+        }
+    }
+}
---
+++ T2009M_AP/lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace T2009M_AP.lab4
+{
+    public delegate void ShowResult(int num1, int num2);
+    public class Program
+    {
+        public static ShowResult sr = new ShowResult(ExDelegate.ShowInfo);
+        public static void Main(string[] args)
+        {
+            ExDelegate ed = new ExDelegate();
+            sr += new ExDelegate().AddNumber;
+            sr += new ExDelegate().SubNumber;
+            sr += new ExDelegate().MulNumber;
+            sr += new ExDelegate().DivNumber;
+            sr(2, 4);
+        }
+    }
+}
